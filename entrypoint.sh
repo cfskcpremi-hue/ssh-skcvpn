12c9a2b808d39f208e350668631a5f803fef1a8b
kancil-imut <cfskcpremi@gmail.com>
2026-09-26 03:14:11 +0700

    Update entrypoint.sh

--- entrypoint.sh
+++ entrypoint.sh
@@ -3,14 +3,14 @@
 # Jalankan SSH server di port internal 2222
 /usr/sbin/sshd
 
-# Jalankan Haproxy untuk menghandle port 80 dan 443
-exec haproxy -f /etc/haproxy/haproxy.cfg &
+# Jalankan HAProxy di background (&) agar tidak menahan proses
+haproxy -f /etc/haproxy/haproxy.cfg &
 
 echo "=================================================="
 echo " SSH Multi-Protocol Server Aktif di Railway"
 echo " User     : ridsvpn"
 echo " Password : kancil"
-echo " Support  : Port 80 & 443 (WS, Stunnel, TLS, HTTPS, HTTP Upgrade)"
+echo " Support  : Port 80 & 443 (WS, Stunnel, TLS, HTTP)"
 echo "=================================================="
 
 # Menjaga container tetap hidup
