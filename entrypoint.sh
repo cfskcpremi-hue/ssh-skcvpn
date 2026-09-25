277a3dbd92792eee87628c2681ca1b775358aa03
kancil-imut <cfskcpremi@gmail.com>
2026-09-26 04:37:55 +0700

    Update entrypoint.sh

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,4 +1,11 @@
 #!/bin/bash
+
+# Buat sertifikat SSL mandiri (.pem)
+openssl req -new -x509 -days 365 -nodes \
+    -out /app/cert.pem \
+    -keyout /app/key.pem \
+    -subj "/C=ID/ST=Jakarta/L=Jakarta/O=SKC/CN=railway.app"
+
 sed -i 's/#Port 22/Port 2222/' /etc/ssh/sshd_config
 /usr/sbin/sshd
 python3 /app/ws.py
