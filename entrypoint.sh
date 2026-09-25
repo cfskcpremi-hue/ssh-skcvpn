f2b5cd10c8cda480ce377cddaff726c0f4dec3d5
kancil-imut <cfskcpremi@gmail.com>
2026-09-26 04:23:05 +0700

    Update entrypoint.sh

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,14 +1,4 @@
 #!/bin/bash
-mkdir -p /var/run/stunnel4 /etc/stunnel
-
-openssl req -new -x509 -days 365 -nodes \
-    -out /etc/stunnel/stunnel.pem \
-    -keyout /etc/stunnel/stunnel.pem \
-    -subj "/C=ID/ST=Jakarta/L=Jakarta/O=SKC/CN=railway.app"
-
-chmod 600 /etc/stunnel/stunnel.pem
-
+sed -i 's/#Port 22/Port 2222/' /etc/ssh/sshd_config
 /usr/sbin/sshd
-stunnel4 /etc/stunnel/stunnel.conf
-
-sleep infinity
+python3 /app/ws.py
